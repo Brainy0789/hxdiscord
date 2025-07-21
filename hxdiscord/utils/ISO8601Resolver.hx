bee9cb6ba42944db18da560ab85062c4f4bff6c8
Brainy7980 <bpurdy12@icloud.com>
2025-07-20 19:42:47 -0700

    fixed bad case hopefully

--- hxdiscord/utils/ISO8601Resolver.hx
+++ hxdiscord/utils/ISO8601Resolver.hx
@@ -1,5 +1,3 @@
-//i'm going to suffer a lot doing this (typed this before doing it)
-
 package hxdiscord.utils;
 
 using StringTools;
@@ -13,50 +11,62 @@ class ISO8601Resolver {
         Returns an ISO8601 formatted string about the current date
     **/
     public static function getActualISODate():String {
-        return DateTools.format(Date.now(), "%Y-%m-%dT%H:%M:%S." + Std.string(haxe.Timer.stamp() * 10000).split(".")[1] + "Z"); //I DON'T KNOW IF THIS WORKS FOR JS, I'LL TRY TO FIX IT ONCE I ADD JS SUPPORT
+        return DateTools.format(Date.now(), "%Y-%m-%dT%H:%M:%S." + Std.string(haxe.Timer.stamp() * 10000).split(".")[1] + "Z"); 
+        // Note: Might need adjustment for JS targets.
     }
 
     /**
         Returns an ISO8601 formatted string about the calculated time you want
         @param time Enter the time you wanna calculate (i.e 10m)
-        @param returnMilliseconds Whether to return miliseconds or not
+        @param returnMilliseconds Whether to return milliseconds or not
     **/
     public static function getCalculatedISODate(time:String, ?returnMilliseconds:Bool):String {
         var dateToReturn:String = "";
-        if (!time.endsWith("s") && !time.endsWith("m") && !time.endsWith("h") && !time.endsWith("d") && !time.endsWith("w") && !time.endsWith("m") && !time.endsWith("y"))
-        {
+
+        var validUnits = ['s', 'm', 'h', 'd', 'w', 'M', 'y']; // lowercase months changed to 'M' to avoid confusion
+        var lastThing:String = time.charAt(time.length - 1);
+
+        if (validUnits.indexOf(lastThing) == -1 && validUnits.indexOf(lastThing.toUpperCase()) == -1) {
             throw "Either you specified an invalid parameter or you didn't specify it";
         }
-        else
-        {
-            var lastThing:String = time.split("")[time.split("").length-1];
-            var theOtherParameter:Float = Std.parseFloat(time.split(lastThing)[0]);
-            var timeToAddInMS:Float = 0;
-            var date:Date = new Date(Date.now().getUTCFullYear(), Date.now().getUTCMonth(), Date.now().getUTCDate(), Date.now().getUTCHours(), Date.now().getUTCMinutes(), Date.now().getUTCSeconds());
-            switch(lastThing.toLowerCase()) {
-                case "s": //seconds i think
-                    timeToAddInMS = 1000;
-                case "m":
-                    timeToAddInMS = 60000;
-                case "h":
-                    timeToAddInMS = 3600000;
-                case "d":
-                    timeToAddInMS = 86400000;
-                case "w":
-                    timeToAddInMS = 24 * 60 * 60 * 1000;
-                case "m":
-                    timeToAddInMS = 2629800000;
-                case "y":
-                    timeToAddInMS = 2629800000 * 12;
-            }
-            if (returnMilliseconds) {
-                dateToReturn = DateTools.format(DateTools.delta(date, timeToAddInMS * theOtherParameter), "%Y-%m-%dT%H:%M:%S." + Std.string(haxe.Timer.stamp() * 10000).split(".")[1] + "Z");
-            }
-            else
-            {
-                dateToReturn = DateTools.format(DateTools.delta(date, timeToAddInMS * theOtherParameter), "%Y-%m-%dT%H:%M:%S.000Z");
-            }
+
+        var theOtherParameter:Float = Std.parseFloat(time.substr(0, time.length - 1));
+        var timeToAddInMS:Float = 0;
+
+        var date:Date = new Date(
+            Date.now().getUTCFullYear(),
+            Date.now().getUTCMonth(),
+            Date.now().getUTCDate(),
+            Date.now().getUTCHours(),
+            Date.now().getUTCMinutes(),
+            Date.now().getUTCSeconds()
+        );
+
+        switch(lastThing.toLowerCase()) {
+            case "s":
+                timeToAddInMS = 1000;
+            case "m": // minutes
+                timeToAddInMS = 60000;
+            case "h":
+                timeToAddInMS = 3600000;
+            case "d":
+                timeToAddInMS = 86400000;
+            case "w":
+                timeToAddInMS = 7 * 86400000; // 7 days in ms
+            case "M": // months (uppercase M) — handle separately
+                timeToAddInMS = 2629800000;
+            case "y":
+                timeToAddInMS = 2629800000 * 12;
         }
+
+        var deltaDate = DateTools.delta(date, timeToAddInMS * theOtherParameter);
+
+        if (returnMilliseconds) {
+            dateToReturn = DateTools.format(deltaDate, "%Y-%m-%dT%H:%M:%S." + Std.string(haxe.Timer.stamp() * 10000).split(".")[1] + "Z");
+        } else {
+            dateToReturn = DateTools.format(deltaDate, "%Y-%m-%dT%H:%M:%S.000Z");
+        }
+
         return dateToReturn;
     }
 }
